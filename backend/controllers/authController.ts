9650f32fa43b23398b94a6fd44b989e7423afcc0
Satria-WS <extencezone02@gmail.com>
2025-01-16 16:39:03 +0700

    Refactor authController for improved code readability and formatting

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -1,5 +1,5 @@
-import User from "../models/userModel";
-import jwt from "jsonwebtoken";
+import User from '../models/userModel';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
 interface Request {
@@ -22,14 +22,18 @@ export const register = async (req: Request, res: Response) => {
     if (!process.env.JWT_SECRET) {
       throw new Error('JWT_SECRET is not defined');
     }
-    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
     res.status(201).json({ token });
   } catch (err: any) {
-    res.status(500).json({ message: 'Error registering user', error: err.message });
+    res
+      .status(500)
+      .json({ message: 'Error registering user', error: err.message });
   }
 };
 
-export const login = async (req:Request, res:Response) => {
+export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   try {
     const user = await User.findByEmail(email);
@@ -45,9 +49,11 @@ export const login = async (req:Request, res:Response) => {
     if (!process.env.JWT_SECRET) {
       throw new Error('JWT_SECRET is not defined');
     }
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.json({ token });
-  } catch (err:any) {
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+    res.json({ token, msg: 'login succesfull' });
+  } catch (err: any) {
     res.status(500).json({ message: 'Error logging in', error: err.message });
   }
 };
