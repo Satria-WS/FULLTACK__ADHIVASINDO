a380193575bce17ba013b36409b4f3f7033de889
Satria-WS <extencezone02@gmail.com>
2025-01-17 18:49:11 +0700

    Refactor server routes to correct import names and add user routes

--- backend/server.ts
+++ backend/server.ts
@@ -2,9 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDb } from "./config/db"; // Import the connectDb function
-import authRoutes from './routes/authRoutes';
+import authRouters from './routes/authRoutes';
 import contentRouters from "./routes/contentRoutes";
-
+import userRouters from "./routes/userRouter";
 
 dotenv.config();
 
@@ -13,8 +13,9 @@ const app = express();
 app.use(express.json());  // Untuk parsing body JSON
 app.use(cookieParser());  // Untuk parsing cookies
 // Rute untuk autentikasi
-app.use('/api/auth', authRoutes);
-app.use('/api',contentRouters)
+app.use('/api/auth', authRouters);
+app.use('/api', contentRouters)
+app.use('/api',userRouters)
 
 connectDb().then(() => {
   app.listen(9000, () => {
