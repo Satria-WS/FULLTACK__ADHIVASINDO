879acd4d8a5ba1979278eb62dae5bda1534f951f
Satria-WS <extencezone02@gmail.com>
2025-01-18 14:11:01 +0700

    add dashboard

--- frontend-desktop/src/components/Home/Dashboard.tsx
+++ frontend-desktop/src/components/Home/Dashboard.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useAuth } from '../Auth/AuthContext';
+import Logout from './Logout';
+
 const Dashboard = () => {
-  const { logout } = useAuth();
+
 
   return (
     <div>
-      <h1>Welcome to the Dashboard</h1>
-      <button onClick={logout}>Logout</button>
+        <Logout/>
     </div>
   );
 };
